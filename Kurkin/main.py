ecb10872203658e8fabc9164b39e7372012e686e
danilakurkin08-sys <danilakurkin08@gmail.com>
2025-11-13 10:17:29 +0300

    Update main.py

--- Kurkin/main.py
+++ Kurkin/main.py
@@ -4,21 +4,42 @@
 # It echoes any incoming text messages.
 
 import telebot
-from telebot.types import Message
-from config import tokenum
-bot = telebot.TeleBot(tokenum)
-print(tokenum)
+from config import API_TOKEN
+from telebot.types import (KeyboardButton,
+                           ReplyKeyboardMarkup,
+                           ReplyKeyboardRemove,
+                           Message)
+
+keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+
+
+button1 = telebot.types.KeyboardButton(text="Кнопка 1")
+button2 = telebot.types.KeyboardButton(text="Кнопка 2")
+keyboard.add(button1)
+keyboard.add(button2)
+keyboard.add('1', '2')
+# keyboard.row('5', '6', '7')
+
+bot = telebot.TeleBot(API_TOKEN)
+print(API_TOKEN)
 
 
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message: Message):
-    bot.reply_to(message, 'Привет!, {message.from_user.first_name}')
+    bot.reply_to(message, f'Привет!, {message.from_user.first_name}')
 
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
     bot.reply_to(message, message.text)
+    bot.send_message(
+        message.chat.id,
+        'hello_message',
+        reply_markup=keyboard,
+        parse_mode='HTML'
+    )
 
 
 bot.infinity_polling()
+
