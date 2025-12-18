9dad33dec475d3d632f00f54d4f415af8afa6955
danilakurkin08-sys <danilakurkin08@gmail.com>
2025-12-18 10:12:54 +0300

    Update main.py

--- Kurkin/main.py
+++ Kurkin/main.py
@@ -1,13 +1,15 @@
 import telebot
-from config import API_TOKEN
 from telebot.types import (KeyboardButton,
                            ReplyKeyboardMarkup,
                            ReplyKeyboardRemove,
                            Message)
 
+API_TOKEN ='8576595076:AAGywQ3XFGagq1Cqben1PoLtQZC1L6RqVoA'
+
+keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
 
 keyboard = ReplyKeyboardMarkup()
-button = KeyboardButton(text="FAAAAAAh")
+button = KeyboardButton(text="да")
 button2 = KeyboardButton(text="дай-ка подумать...нет")
 
 keyboard.add(button)
@@ -28,35 +30,43 @@ def send_welcome(message: Message):
         hello_message,
         reply_markup=keyboard,
         parse_mode='HTML' 
-
+    )
 state = 1
 
-  @bot.message_handler(func=lambda message: True)
+@bot.message_handler(func=lambda message: True)
 def echo_message(message: Message):
     global state
-    if message.text == "FAAAAAAh" and state == 1:
+    if message.text == "да" and state == 1:
+        
         bot.send_message(
             message.from_user.id,
             'Ну тогда давай сыграем в игру?',
+            reply_markup=ReplyKeyboardMarkup().add("ну давай","не хочу" )
         )
+
+        state = 2
+
     elif message.text == "дай-ка подумать...нет" and state == 1:
         bot.send_message(
             message.from_user.id,
             'тогда надо познакомиться, меня зовут Роберт, давай поиграем?',
+             reply_markup=ReplyKeyboardMarkup().add("ну давай","не хочу" )
         )
-      
-    state = 2
 
-  elif message.text == "давай" and state == 2:
+    elif message.text == "ну давай" and state == 2:
         bot.send_message(
             message.from_user.id,
             'отлично, выбери в какую в дверь пойдешь:в левую или в правую?',
+            reply_markup=ReplyKeyboardMarkup().add("левая","правая")
         )
-  elif message.text == "давай" and state == 1:
+            
+    elif message.text == "ну давай" and state == 2:
         bot.send_message(
             message.from_user.id,
             'отлично, выбери в какую в дверь пойдешь:в левую или в правую?',
+            reply_markup=ReplyKeyboardMarkup().add("левая","правая")
         )
+            
     else:
         bot.send_message(
             message.from_user.id,
@@ -66,4 +76,3 @@ def echo_message(message: Message):
 
 
 bot.infinity_polling()
-
